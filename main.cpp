8df70a8215a4f00e7cdbe83064292937913952b2
RafaelDaEngineer <181130101+RafaelDaEngineer@users.noreply.github.com>
2026-05-02 19:35:09 +0200

    Chapter 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cassert>
+#include <sstream>
 
 void variablesExample() {
     int number{};
@@ -32,9 +34,58 @@ void inputExperiment() {
     }
 }
 
+[[nodiscard]] bool getNumber(std::istream& inputStream, double number) {
+    inputStream >> number;
+    if (inputStream) {
+        return true;
+    } else {
+        inputStream.clear();
+        inputStream.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return false;
+    }
+}
+
+void someExperiment() {
+    double number {};
+    bool OK = getNumber(std::cin, number);
+    std::cout << OK << std::endl;
+}
+
+void testCode() {
+    double value{};
+    std::stringstream someInput{"1"};
+    const bool ok = getNumber(someInput, value);
+    assert(ok);
+    assert(value == 1);
+
+    double unused{};
+    std::stringstream badInput{"a"};
+    const bool notOk = getNumber(badInput, unused);
+    assert(!notOk);
+}
+
+
 // Compiling a program usually follows this template:
 // tool_name [optional flags] source_name.cpp -o output_name
 int main() {
+    testCode();
+
+    double number{};
+    std::cout << "Please enter a number: \n";
+    const bool ok = getNumber(std::cin, number);
+    if (ok) {
+        std::cout << "You entered: " << number << std::endl;
+    } else {
+        std::cout << "Error" << std::endl;
+        std::cout << "Please try again" << std::endl;
+        const bool okNow = getNumber(std::cin, number);
+        if (okNow) {
+            std::cout << "You entered: " << number << std::endl;
+        } else {
+            std::cout << "Error again" << std::endl;
+        }
+    }
+
 
     return 0;
 }
